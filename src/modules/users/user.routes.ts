6250bde2e62edadc94156df8db11f4e63f42ecd3
mehul-r-7span <mehul.r@7span.com>
2026-03-18 16:09:12 +0530

    feat: add authorizeUserAccess middleware for user access control

--- src/modules/users/user.routes.ts
+++ src/modules/users/user.routes.ts
@@ -3,6 +3,7 @@ import { Router } from "express";
 import ROLES from "../../constants/roles.js";
 import authenticate from "../../middlewares/authenticate.middleware.js";
 import authorize from "../../middlewares/authorize.middleware.js";
+import authorizeUserAccess from "../../middlewares/authorizeUserAccess.middleware.js";
 import validate from "../../middlewares/validate.middleware.js";
 
 import { deleteUser, getAllUsers, getUser, updateUser } from "./user.controller.js";
@@ -11,8 +12,8 @@ import { updateUserSchema } from "./user.validation.js";
 const router = Router();
 
 router.get("/", authenticate, authorize(ROLES.ADMIN), getAllUsers);
-router.get("/:id", authenticate, getUser);
-router.put("/:id", authenticate, validate(updateUserSchema), updateUser);
-router.delete("/:id", authenticate, deleteUser);
+router.get("/:id", authenticate, authorizeUserAccess, getUser);
+router.put("/:id", authenticate, authorizeUserAccess, validate(updateUserSchema), updateUser);
+router.delete("/:id", authenticate, authorizeUserAccess, deleteUser);
 
 export default router;
